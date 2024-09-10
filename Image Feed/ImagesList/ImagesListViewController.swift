46155b50e7d4221950efacf2ced7468a8fb0a3f8
elc <hm8huh@mail.ru>
2024-09-10 19:43:34 +0300

    Добавлены изменения исходя из ревью

--- Image Feed/ImagesList/ImagesListViewController.swift
+++ Image Feed/ImagesList/ImagesListViewController.swift
@@ -15,7 +15,6 @@ final class ImagesListViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        tableView.rowHeight = 200
         tableView.contentInset = UIEdgeInsets(top: 12, left: 0, bottom: 12, right: 0)
     }
     
